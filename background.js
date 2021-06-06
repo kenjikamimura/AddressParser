2f143a03b583d63df86913ce3b61178dd5d85ff0
Kenji Kamimura <kjp.kamimura@gmail.com>
2021-06-06 21:06:01 +1200

    Get the address details using a keyboard shortcut

--- background.js
+++ background.js
@@ -1,6 +1,35 @@
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
+chrome.commands.onCommand.addListener(function (command) {
+  switch (command) {
+    case "get-details":
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: getDetails,
+        });
+      });
+      break;
+    default:
+      console.log(`Command ${command} not found`);
+  }
 });
+
+function getDetails() {
+  const getName = () => {
+    return document.querySelector("input[name='name']").value;
+  };
+
+  const getCompany = () => {
+    return document.querySelector("input[name='company']").value;
+  };
+
+  const getAddress = () => {
+    return document.querySelector("textarea[name='address']").value;
+  };
+
+  const customerName = getName();
+  const companyName = getCompany();
+  const address = getAddress();
+  console.log("name:", customerName);
+  console.log("companyName:", companyName);
+  console.log("address:", address);
+}
--- popup.js
+++ popup.js
@@ -1,24 +1,38 @@
-// Initialize button with user's preferred color
 let changeColor = document.getElementById("changeColor");
 
 chrome.storage.sync.get("color", ({ color }) => {
   changeColor.style.backgroundColor = color;
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-	chrome.scripting.executeScript({
-	  target: { tabId: tab.id },
-	  function: setPageBackgroundColor,
-	});
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
   });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-	chrome.storage.sync.get("color", ({ color }) => {
-	  document.body.style.backgroundColor = color;
-	});
-  }
+});
+
+function setPageBackgroundColor() {
+  const getName = () => {
+    return document.querySelector("input[name='name']").value;
+  };
+
+  const getCompany = () => {
+    return document.querySelector("input[name='company']").value;
+  };
+
+  const getAddress = () => {
+    return document.querySelector("textarea[name='address']").value;
+  };
+
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+    const customerName = getName();
+    const companyName = getCompany();
+    const address = getAddress();
+    console.log("name:", customerName);
+    console.log("companyName:", companyName);
+    console.log("address:", address);
+  });
+}
