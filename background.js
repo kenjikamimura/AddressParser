c7e523a2075f41334fc4cdd133398d526137d2d0
Kenji Kamimura <kjp.kamimura@gmail.com>
2021-06-06 21:39:42 +1200

    Add functionality to add name to fastway website

--- background.js
+++ background.js
@@ -7,18 +7,30 @@ chrome.commands.onCommand.addListener(async function (command) {
       });
       console.log("tabs:", activeTab);
 
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        function: getDetails,
-      });
+      const baseUrl = activeTab.url.split("/")[2];
+      console.log("baseUrl:", baseUrl);
 
+      switch (baseUrl) {
+        case "omins.snipesoft.net.nz":
+          chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            function: getDetails,
+          });
+          break;
+        case "myfastway.co.nz":
+          chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            function: pasteDetails,
+          });
+          break;
+      }
       break;
     default:
       console.log(`Command ${command} not found`);
   }
 });
 
-function getDetails() {
+async function getDetails() {
   const getName = () => {
     return document.querySelector("input[name='name']").value;
   };
@@ -35,6 +47,20 @@ function getDetails() {
   const companyName = getCompany();
   const address = getAddress();
   console.log("name:", customerName);
+  await chrome.storage.sync.set({ customerName: customerName });
+
   console.log("companyName:", companyName);
   console.log("address:", address);
+}
+
+async function pasteDetails() {
+  const name = await new Promise((resolve) => {
+    chrome.storage.sync.get(["customerName"], (options) => {
+      resolve(options.customerName);
+    });
+  });
+  console.log("name:", name);
+
+  const inputPanel = document.querySelectorAll(".panel")[1];
+  inputPanel.querySelector("input[formcontrolname='contactName']").value = name;
 }
